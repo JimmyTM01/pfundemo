822ee8f7e9eb075fea118935e0c58228352c664f
JimmyTM01 <jimmypattinson01@gmail.com>
2026-03-15 21:59:26 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import angular from '@analogjs/vite-plugin-angular';
+
+export default defineConfig({
+  plugins: [
+    angular({
+      tsconfig: './tsconfig.app.json'
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
+});
